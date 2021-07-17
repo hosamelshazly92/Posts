ad6c0dccf359a83e3ae178a219b4bdc212cbf516
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-07-17 22:00:41 +0200

    set up database connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,26 @@
-import path from "path";
-import express from "express";
-import mongoose from "mongoose";
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+mongoose.connect(process.env.DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("==========> database success");
+}).catch((err) => {
+    console.log(err);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("==========> server success");
+});

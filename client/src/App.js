8d7ec4640a71a9cd553974c04ed453befca2fde8
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-07-18 00:03:23 +0200

    create Form, Posts and Post components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,54 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import moments from "./img/moments.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 const App = () => {
     return (
-        <div>
-            <h1>el3ab yabo 3amo</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar 
+                position="static" 
+                color="inherit"
+            >
+                <Typography 
+                    variant="h2" 
+                    align="center"
+                >
+                    Moments
+                </Typography>
+                <img 
+                    src={moments} 
+                    alt="moments" 
+                    height="600" 
+                />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid 
+                        container 
+                        justify="space-between" 
+                        alignItems="stretch" 
+                        spacing={ 3 }
+                    >
+                        <Grid 
+                            item 
+                            xs={ 12 }
+                            sm={ 7 }
+                        >
+                            <Posts />
+                        </Grid>
+                        <Grid 
+                            item 
+                            xs={ 12 }
+                            sm={ 4 }
+                        >
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
